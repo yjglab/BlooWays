7318cb4eb4ede2e8df4ccf694868836422f30727
yjglab <yjgdesign@gmail.com>
2023-05-20 02:06:59 +0900

    Add: local passport, base router

---
+++ back/passport/index.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const local = require("./local");
+const Blooway = require("../models/blooway");
+const User = require("../models/user");
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({
+        where: { id },
+        attributes: ["id", "email", "username"],
+        include: [
+          {
+            model: Blooway,
+            as: "Blooways",
+          },
+        ],
+      });
+      done(null, user);
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
+  });
+  local();
+  // kakao
+  // google
+};
