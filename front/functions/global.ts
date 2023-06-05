460f85f69a1248a5367e708aafd89f77ea2ec736
yjglab <yjgdesign@gmail.com>
2023-06-05 23:24:07 +0900

    Fix: iamge path

--- front/functions/global.ts
+++ front/functions/global.ts
@@ -1,9 +1,13 @@
 import { ToastPosition } from 'react-toastify';
 const isProdiction = process.env.NODE_ENV === 'production';
 export const backUrl = isProdiction ? 'https://blooways.online' : 'http://localhost:4094';
-export const logoUrl = '/public/images/blooways_logo.png';
-export const yjglabLogoUrl = '/public/images/yjglab_logo_slate500.png';
-export const blooboltFullLogoUrl = '/public/images/bloobolt-full-logo.png';
+export const logoUrl = isProdiction ? '/images/blooways_logo.png' : '/public/images/blooways_logo.png';
+export const yjglabLogoUrl = isProdiction
+  ? '/images/yjglab_logo_slate500.png'
+  : '/public/images/yjglab_logo_slate500.png';
+export const blooboltFullLogoUrl = isProdiction
+  ? '/images/bloobolt-full-logo.png'
+  : '/public/images/bloobolt-full-logo.png';
 
 interface ToastConfig {
   position: ToastPosition | undefined;
